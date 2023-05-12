3e46b33b8d07a734326b19114be73437d9ae2ca6
danteboe <juandavidtap38@gmail.com>
2023-05-11 21:59:26 -0500

    First commit of the project.

---
+++ script.js
@@ -0,0 +1,321 @@
+const images = document.getElementsByTagName("img");
+const clothing = [
+  {
+    name: 'Crop-top rosa',
+    tags: 'veraniego;rosa;crop-top;',
+    link: 'crop_top_rosa.png"'
+  },
+  {
+    name: 'camiseta-negra',
+    tags: 'basica;negro;camiseta;',
+    link: 'camiseta_negra.png"'
+  },
+  {
+    name: 'camiseta-blanca',
+    tags: 'basica;blanco;camiseta;',
+    link: 'camiseta_blanca.png"'
+  },
+  {
+    name: 'camiseta-verde',
+    tags: 'basica;verde;camiseta;',
+    link: 'camiseta_verde.png"'
+  },
+  {
+    name: 'camiseta-gris',
+    tags: 'basica;gris;camiseta;',
+    link: 'camiseta_gris.png"'
+  },
+  {
+    name: 'cadena cubana',
+    tags: 'elegante;gris;cadena;',
+    link: 'cadena_cubana.png"'
+  },
+  {
+    name: 'camisa -blanca',
+    tags: 'clásico;blanco;camisa;',
+    link: 'camisablanca.png"'
+  },
+  {
+    name: 'pantalones vaqueros',
+    tags: 'casual;azul;vaqueros;',
+    link: 'pantalones_vaqueros.png"'
+  },
+  {
+    name: 'sombrero de paja',
+    tags: 'veraniego;marrón;sombrero;',
+    link: 'sombrero_paja.png"'
+  },
+  {
+    name: 'collar de perlas',
+    tags: 'elegante;blanco;perlas;',
+    link: 'collar_perlas.png"'
+  },
+  {
+    name: 'vestido negro',
+    tags: 'formal;negro;vestido;',
+    link: 'vestido_negro.png"'
+  },
+  {
+    name: 'shorts de mezclilla',
+    tags: 'casual;mezclilla;shorts;',
+    link: 'shorts_mezclilla.png"'
+  },
+  {
+    name: 'hoodie negro',
+    tags: 'hoodie;basico;negro;',
+    link: 'hoodie_negro.png"'
+  },
+  {
+    name: 'bufanda de lana',
+    tags: 'invierno;marrón;bufanda;',
+    link: 'bufanda_lana.png"'
+  },
+  {
+    name: 'jogger gris',
+    tags: 'casual;basico;gris;',
+    link: 'jogger_gris.png"'
+  },
+  {
+    name: 'suéter de cuello alto',
+    tags: 'invierno;gris;suéter;',
+    link: 'sweater_cuello_alto.png"'
+  },
+  {
+    name: 'falda plisada',
+    tags: 'formal;rosa;falda;',
+    link: 'falda_plisada.png"'
+  },
+  {
+    name: 'gorro de lana',
+    tags: 'invierno;rojo;gorro;',
+    link: 'gorro_lana.png"'
+  },
+  {
+    name: 'hoodie rojo estampado',
+    tags: 'rojo;nike;hoodie;casual;',
+    link: 'hoodie_estampado.png"'
+  },
+  {
+    name: 'cinturón de cuero',
+    tags: 'marrón;cuero;cinturón;',
+    link: 'cinturon_cuero.png"'
+  },
+  {
+    name: 'blusa de encaje blanco',
+    tags: 'romántico;blanco;encaje;blusa;',
+    link: 'blusa_encaje_blanco.png"'
+  },
+  {
+    name: 'leggings negros',
+    tags: 'deportivo;negro;leggins;',
+    link: 'leggins_negros.png"'
+  },
+  {
+    name: 'pañuelo estampado',
+    tags: 'veraniego;colorido;pañuelo;',
+    link: 'pañuelo_estampado.png"'
+  },
+  {
+    name: 'zapatos de tacón alto',
+    tags: 'elegante;negro;tacón;zapatos;',
+    link: 'tacos_negros.png"'
+  },
+  {
+    name: 'bolsa de cuero',
+    tags: 'marrón;cuero;bolsa;',
+    link: 'bolsa_cuero_marron.png"'
+  },
+  {
+    name: 'boina de lana',
+    tags: 'invierno;rojo;boina;',
+    link: 'boina_lana_roja.png"'
+  },
+  {
+    name: 'bralette negro',
+    tags: 'romantico;negro;bralette;',
+    link: 'bralette_negro.png"'
+  },
+  {
+    name: 'zapatos mocasines',
+    tags: 'negro;elegante;zapatos;',
+    link: 'mocasines_negros.png"'
+  },
+  {
+    name: 'tenis blancos',
+    tags: 'casual;blanco;tenis;',
+    link: 'tenis_blancos.png"'
+  },
+  {
+    name: 'vestido de verano',
+    tags: 'veraniego;floral;vestido;',
+    link: 'vestido_floral.png"'
+  },
+  {
+    name: 'chaqueta de cuero',
+    tags: 'negro;elegante;chaqueta;',
+    link: 'chaqueta_cuero.png"'
+  },
+  {
+    name: 'chaqueta de plumas',
+    tags: 'blanco;casual;chaqueta;',
+    link: 'chaqueta_plumas.png"'
+  },
+  { name: 'jean con rotos', tags: 'casual;jean;', link: 'jean.png"' },
+  {
+    name: 'botas de cuero',
+    tags: 'marrón;cuero;botas;',
+    link: 'botas_cuero_marron.png"'
+  },
+  {
+    name: 'gorra de béisbol',
+    tags: 'casual;azul;gorra;',
+    link: 'gorra_beisbol_azul.png"'
+  },
+  {
+    name: 'sandalias de tacón',
+    tags: 'elegante;dorado;tacón;sandalias;',
+    link: 'sandalias_tacon_dorado.png"'
+  },
+  {
+    name: 'pendientes de plata',
+    tags: 'plata;pendientes;',
+    link: 'pendientes_plata.png"'
+  },
+  {
+    name: 'blusa de seda',
+    tags: 'elegante;rosa;seda;blusa;',
+    link: 'blusa_seda_rosa.png"'
+  },
+  {
+    name: 'falda plisada',
+    tags: 'elegante;negro;plisada;falda;',
+    link: 'falda_plisada_negra.png"'
+  },
+  {
+    name: 'botines negros',
+    tags: 'casual;negro;botines;',
+    link: 'botines_negros.png"'
+  },
+  {
+    name: 'camiseta sin mangas',
+    tags: 'casual;blanco;camiseta;',
+    link: 'camiseta_blanca.png"'
+  },
+  {
+    name: 'bolso de mano',
+    tags: 'casual;azul;bolso;',
+    link: 'bolso_mano_azul.png"'
+  },
+  {
+    name: 'jeans pitillo',
+    tags: 'casual;azul;jeans;',
+    link: 'jeans_pitillo_azul.png"'
+  },
+  {
+    name: 'blusa sin mangas',
+    tags: 'verano;amarillo;blusa; sin mangas;',
+    link: 'blusa_sin_mangas_amarilla.png"'
+  },
+  {
+    name: 'pantalón de vestir',
+    tags: 'elegante;gris;pantalón;',
+    link: 'pantalón_vestir_gris.png"'
+  },
+  {
+    name: 'reloj de pulsera',
+    tags: 'casual;plateado;reloj;',
+    link: 'reloj_pulsera_plateado.png"'
+  },
+  {
+    name: 'Bolso bandolera',
+    tags: 'casual;rojo;bolso bandolera;',
+    link: 'bolso_bandolera_rojo.png"'
+  },
+  {
+    name: 'mocasines de ante',
+    tags: 'elegante;azul;ante;mocasines;',
+    link: 'mocasines_ante_azul.png"'
+  },
+  {
+    name: 'chaqueta de cuero',
+    tags: 'elegante;negro;chaqueta;cuero;',
+    link: 'chaqueta_cuero_negro.png"'
+  },
+  {
+    name: 'pantalones cargo',
+    tags: 'casual;khaki;pantalones cargo;',
+    link: 'pantalones_cargo_khaki.png"'
+  },
+  {
+    name: 'tennis de bota',
+    tags: 'blanco;tennis de bota;casual;',
+    link: 'tennis_bota.png"'
+  },
+  {
+    name: 'chaqueta rompevientos',
+    tags: 'deportivo;chaqueta;',
+    link: 'chaqueta_rompevientos.png"'
+  }
+]
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", function() {
+    if (this.classList.contains("image_unselected")) {
+      this.classList.remove("image_unselected");
+      this.classList.add("image_selected");
+    } else {
+      this.classList.remove("image_selected");
+      this.classList.add("image_unselected");
+    }
+  });
+}
+
+const bestStyles = [];
+const tags_selected =[];
+const counted_tags = {};
+
+function collect_tags(){
+  
+  const selected = document.getElementsByClassName("image_selected");
+  
+  for (let i = 0; i<selected.length;i++){
+    const categories = clothing[selected[i].id]['tags'].split(";");
+
+    for (let i= 0; i<categories.length; i++){
+      tags_selected.push(categories[i])
+    }
+  }
+  for (let i = 0; i<tags_selected.length;i++){
+    if (!(tags_selected[i] in Object.keys(counted_tags))){
+      counted_tags[tags_selected[i]] = 1
+    }
+    else{
+      counted_tags[tags_selected[i]]+=1
+    }
+  }
+  
+  var keys = Object.keys(counted_tags);
+  var styles= counted_tags
+  for (let i = 0; i< keys.length-1; i++){
+      if (keys[i]!=''){if (bestStyles.length!=3){
+          bestStyles.push(keys[i])
+      }
+      else if (styles[keys[i]]>styles[bestStyles[0]]){
+          bestStyles[0]=keys[i]
+      }
+      else if (styles[keys[i]]>styles[bestStyles[1]]){
+          bestStyles[1]=keys[i]
+      }
+      else if (styles[keys[i]]>styles[bestStyles[2]]){
+          bestStyles[2]=keys[i]
+      }
+  }
+  
+}
+var answers = document.getElementById('results');
+
+answers.innerHTML += ('Las prendas/estilos que más te quedan son:'+bestStyles.toString());
+}
+const doneButton = document.getElementById('done_button');
+doneButton.addEventListener('click', collect_tags);
+
---
+++ survey_script.cjs
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const { JSDOM } = require("jsdom");
+
+const port = process.env.PORT || 4000;
+const indexHtml = fs.readFileSync('./survey.html', 'utf-8');
+const browserify = require('browserify');
+
+app.use(express.static('C:/Users/JUAN DAVID/Documents/Projects/betostyleSurvey'));
+
+app.get('/', (req, res) => {
+  const dom = new JSDOM(indexHtml);
+  const document = dom.window.document;
+  const elements = document.getElementsByClassName("column");
+  const images = document.getElementsByTagName("img");
+  const urls = [];
+  
+  const clothing = csvToArrayOfObjects('./clothes.csv');
+  
+
+
+
+  for (var i = 0; i<clothing.length; i++){
+      urls.push(clothing[i]['link']);
+      
+  }
+
+
+  function getImageTag(img) {
+      var randomIndex = Math.floor(Math.random() * urls.length);
+      var link = '/img_clothes/' + urls[randomIndex];
+      img.setAttribute('src', link.slice(0, -1));
+      img.setAttribute('id', [randomIndex])
+      
+  }
+
+
+  for (var i = 0; i < images.length; i++) {
+      getImageTag(images[i]);
+  }
+
+ 
+
+  res.send(dom.serialize());
+});
+
+function csvToArrayOfObjects(filename) {
+  const fileContents = fs.readFileSync(filename, 'utf-8');
+  const rows = fileContents.trim().split('\n');
+  const headers = rows[0].split(',');
+  const result = [];
+
+  for (let i = 1; i < rows.length; i++) {
+    const values = rows[i].split(',');
+    const obj = {};
+
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j].trim()] = values[j].trim();
+    }
+
+    result.push(obj);
+  }
+
+  return result;
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
